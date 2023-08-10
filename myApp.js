bc63830f487e937e550283b4c35b65eacb22b47b
Breno Peixoto <brenosirufo@gmail.com>
2023-08-10 12:10:04 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 let helmet = require('helmet');
-
+app.use(helmet.hidePoweredBy('express'))
+app.use(helmet.frameguard({
+  action: 'deny'
+}))
+app.use(helmet.xssFilter())
+app.use(helmet.noSniff())
+app.use(helmet.ieNoOpen())
+const ninetyDaysInSeconds = 90*24*60*60
+app.use(helmet.hsts({
+  maxAge: ninetyDaysInSeconds,
+  force: true
+}))
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.noCache())
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "trusted-cdn.com"]
+  }
+}))
 
 
 
